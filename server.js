4d8c01273a28f8375089902eeecc7633abb8314c
= <=>
2020-05-19 17:47:20 +0200

    try for cors

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ const PORT = process.env.HTTP_PORT || 8081;
 const app = express();
 
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(bodyParser.json());
 
